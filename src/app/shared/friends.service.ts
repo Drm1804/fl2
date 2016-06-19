1151db805ff935b76fdd3d5415369a719888badc
Drm1804 <drm-a@yandex.ru>
2016-06-19 08:29:54 +0500

    Сделал авторизацию

--- src/app/shared/friends.service.ts
+++ src/app/shared/friends.service.ts
@@ -1,12 +1,34 @@
+///<reference path="../globals.d.ts"/>
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FriendsService {
 
-  constructor() {}
+    constructor() {
+        this.init();
+    }
 
-  checkStatusLogin(){
-    console.log('Проверяем пользователя')
-  }
+
+    init() {
+        VK.init({
+            apiId: 4986848
+        });
+    }
+
+
+    checkStatusLogin() {
+        VK.Auth.getLoginStatus((response)=> {
+            if (response.session && localStorage.getItem('user.ses') !== null) {
+            } else {
+                VK.Auth.login((response) => {
+                    if (response.session) {
+                        localStorage.setItem('user.ses', JSON.stringify(response.session));
+                    } else {
+                        return false;
+                    }
+                });
+            }
+        })
+    }
 
 }
